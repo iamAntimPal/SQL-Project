6cbdbede8ad5775081c7647ef139c2408c745c68
Antim Pal <134076504+iamAntimPal@users.noreply.github.com>
2025-05-31 12:32:54 +0530

    adding some problem solution

--- Budget Project pratice/Expense.sql
+++ Budget Project pratice/Expense.sql
@@ -22,3 +22,35 @@ INSERT INTO expenses (product,description,amount,date)
 VALUES ('food',"order that food",'2000','2020-02-01');
 
 SELECT * FROM expenses;
+
+SELECT * FROM expenses WHERE product = 'food';
+
+SELECT DISTINCT product,description,amount,date
+ FROM expenses;
+
+SELECT * FROM expenses WHERE amount > 2000;
+select id,product,description,amount,date
+FROM expenses
+WHERE amount > 2000
+ORDER BY amount DESC;
+select id,product,description,max(amount),date
+FROM expenses
+GROUP BY id,product,description
+ORDER BY amount DESC
+LIMIT 1;
+select id,product,description,amount,date
+FROM expenses
+WHERE amount > 2000
+ORDER BY amount DESC
+LIMIT 1;
+select id,product,description,amount,date
+FROM expenses
+WHERE amount > 2000
+ORDER BY amount DESC
+LIMIT 1,1;
+select id,product,description,amount,date
+FROM expenses
+WHERE amount > 2000
+ORDER BY amount DESC
+LIMIT 1,1,1;
+select id,product
